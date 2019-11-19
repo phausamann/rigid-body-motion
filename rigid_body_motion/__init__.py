19dee7cd34ac212fa55a3d43fad64e5be38ed6f4
phausamann <peter.hausamann@tum.de>
2019-11-19 15:51:05 +0100

    Add DataArray support

--- rigid_body_motion/__init__.py
+++ rigid_body_motion/__init__.py
@@ -11,7 +11,8 @@ from rigid_body_motion.coordinate_systems import \
 from rigid_body_motion.reference_frames import \
     register_frame, deregister_frame, clear_registry, ReferenceFrame
 from rigid_body_motion.reference_frames import _registry as _rf_registry
-from rigid_body_motion.utils import qmean, rotate_vectors
+from rigid_body_motion.utils import \
+    qmean, rotate_vectors, _maybe_unpack_dataarray, _make_dataarray
 
 try:
     import rigid_body_motion.ros as ros
@@ -20,6 +21,9 @@ except ImportError:
 
 __all__ = [
     'transform',
+    'transform_points',
+    'transform_quaternions',
+    'transform_vectors',
     # coordinate system transforms
     'cartesian_to_polar',
     'polar_to_cartesian',
@@ -31,7 +35,8 @@ __all__ = [
     'clear_registry',
     'ReferenceFrame',
     # utils
-    'qmean'
+    'qmean',
+    'rotate_vectors',
 ]
 
 _cs_funcs = {
@@ -83,7 +88,8 @@ def transform(arr, outof=None, into=None, axis=-1, **kwargs):
     return transformation_func(arr, axis=axis, **kwargs)
 
 
-def transform_vectors(arr, outof=None, into=None, axis=-1):
+def transform_vectors(
+        arr, outof=None, into=None, dim=None, axis=None, timestamps=None):
     """ Transform an array of vectors between reference frames.
 
     Parameters
@@ -99,18 +105,29 @@ def transform_vectors(arr, outof=None, into=None, axis=-1):
         The name of a registered reference frame in which the array will be
         represented after the transformation.
 
-    axis: int, default -1
+    axis: int, optional
         The axis of the array representing the coordinates of the vectors.
+        Defaults to the last axis of the array.
 
     Returns
     -------
     arr_transformed: array_like
         The transformed array.
     """
-    return _rf_registry[outof].transform_vectors(arr, into, axis=axis)
+    arr, axis, ts_in, coords, dims = _maybe_unpack_dataarray(
+        arr, dim=dim, axis=axis, timestamps=timestamps)
+
+    arr, ts_out = ReferenceFrame._resolve(outof).transform_vectors(
+        arr, into, axis=axis, timestamps=ts_in, return_timestamps=True)
+
+    if coords is not None:
+        return _make_dataarray(arr, coords, dims, timestamps, ts_out)
+    else:
+        return arr
 
 
-def transform_points(arr, outof=None, into=None, axis=-1):
+def transform_points(
+        arr, outof=None, into=None, dim=None, axis=None, timestamps=None):
     """ Transform an array of points between reference frames.
 
     Parameters
@@ -126,18 +143,29 @@ def transform_points(arr, outof=None, into=None, axis=-1):
         The name of a registered reference frame in which the array will be
         represented after the transformation.
 
-    axis: int, default -1
+    axis: int, optional
         The axis of the array representing the coordinates of the points.
+        Defaults to the last axis of the array.
 
     Returns
     -------
     arr_transformed: array_like
         The transformed array.
     """
-    return _rf_registry[outof].transform_points(arr, into, axis=axis)
+    arr, axis, ts_in, coords, dims = _maybe_unpack_dataarray(
+        arr, dim=dim, axis=axis, timestamps=timestamps)
+
+    arr, ts_out = ReferenceFrame._resolve(outof).transform_points(
+        arr, into, axis=axis, timestamps=ts_in, return_timestamps=True)
+
+    if coords is not None:
+        return _make_dataarray(arr, coords, dims, timestamps, ts_out)
+    else:
+        return arr
 
 
-def transform_quaternions(arr, outof=None, into=None, axis=-1):
+def transform_quaternions(
+        arr, outof=None, into=None, dim=None, axis=None, timestamps=None):
     """ Transform an array of quaternions between reference frames.
 
     Parameters
@@ -153,12 +181,22 @@ def transform_quaternions(arr, outof=None, into=None, axis=-1):
         The name of a registered reference frame in which the array will be
         represented after the transformation.
 
-    axis: int, default -1
+    axis: int, optional
         The axis of the array representing the coordinates of the quaternions.
+        Defaults to the last axis of the array.
 
     Returns
     -------
     arr_transformed: array_like
         The transformed array.
     """
-    return _rf_registry[outof].transform_quaternions(arr, into, axis=axis)
+    arr, axis, ts_in, coords, dims = _maybe_unpack_dataarray(
+        arr, dim=dim, axis=axis, timestamps=timestamps)
+
+    arr, ts_out = ReferenceFrame._resolve(outof).transform_quaternions(
+        arr, into, axis=axis, timestamps=ts_in, return_timestamps=True)
+
+    if coords is not None:
+        return _make_dataarray(arr, coords, dims, timestamps, ts_out)
+    else:
+        return arr

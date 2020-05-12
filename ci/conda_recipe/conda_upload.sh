ce15a43cd5985aa936fc3ae6aaace5755e2d2de1
Peter Hausamann <peter.hausamann@tum.de>
2020-05-12 16:41:28 +0200

    Update travis config

--- ci/conda_recipe/conda_upload.sh
+++ ci/conda_recipe/conda_upload.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 PKG_NAME=rigid-body-motion
 USER=phausamann
 
-conda activate base
 anaconda -t $ANACONDA_TOKEN upload -u $USER $CONDA_BLD_PATH/noarch/$PKG_NAME*.tar.bz2 --force

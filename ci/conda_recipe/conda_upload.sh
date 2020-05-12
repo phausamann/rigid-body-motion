d81b025fd5d96d49636cdd0578754eefd902bf7e
Peter Hausamann <peter.hausamann@tum.de>
2020-05-12 17:00:36 +0200

    Update conda_upload.sh

--- ci/conda_recipe/conda_upload.sh
+++ ci/conda_recipe/conda_upload.sh
@@ -3,4 +3,6 @@
 PKG_NAME=rigid-body-motion
 USER=phausamann
 
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+conda activate base
 anaconda -t $ANACONDA_TOKEN upload -u $USER $CONDA_BLD_PATH/noarch/$PKG_NAME*.tar.bz2 --force

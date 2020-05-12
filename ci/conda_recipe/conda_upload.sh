d5da8b39cc451f25190c7bcf47d2acf382d1c99b
Peter Hausamann <peter.hausamann@tum.de>
2020-05-12 14:59:42 +0200

    Test conda deployment

--- ci/conda_recipe/conda_upload.sh
+++ ci/conda_recipe/conda_upload.sh
@@ -3,4 +3,4 @@
 PKG_NAME=rigid-body-motion
 USER=phausamann
 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/noarch/$PKG_NAME*.tar.bz2 --force
+anaconda -t $ANACONDA_TOKEN upload -u $USER $CONDA_BLD_PATH/noarch/$PKG_NAME*.tar.bz2 --force

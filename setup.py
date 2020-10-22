5ed85bc3eaf67e1ab493357c7dbd26681d8a4299
Peter Hausamann <peter.hausamann@tum.de>
2020-10-22 16:21:14 +0200

    Fix formatting issue in HISTORY.rst

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/x-rst",
     include_package_data=True,
     keywords="rigid_body_motion",
     name="rigid-body-motion",

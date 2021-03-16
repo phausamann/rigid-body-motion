8f0412f61616ae57a19b21297cf7d4d0253854f1
Peter Hausamann <peter.hausamann@tum.de>
2021-03-16 17:49:42 +0100

    Add nbsphinx_prolog

--- docs/conf.py
+++ docs/conf.py
@@ -47,10 +47,18 @@ extensions = [
 ]
 
 autosummary_generate = True
+
 # numpydoc_class_members_toctree = True
 numpydoc_show_class_members = False
+
 nbsphinx_kernel_name = "python3"
 nbsphinx_execute = "always"
+nbsphinx_prolog = """
+.. nbinfo::
+
+    You can :download:`download <{{ env.docname }}.ipynb>` this guide as a
+    Jupyter notebook.
+"""
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]

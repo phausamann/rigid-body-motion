bf4447fc95361f9519607f05d658f79a09c2edbb
Peter Hausamann <peter.hausamann@tum.de>
2021-05-27 18:48:37 +0200

    Bump version: 0.7.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     name="rigid-body-motion",
     packages=find_packages(exclude=["tests"]),
     url="https://github.com/phausamann/rigid-body-motion",
-    version="0.7.0",
+    version="0.8.0",
     zip_safe=False,
 )

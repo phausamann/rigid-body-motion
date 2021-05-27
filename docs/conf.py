3c3359dd83952dc7616d4146a39ada1641dbba47
Peter Hausamann <peter.hausamann@tum.de>
2021-05-27 15:48:21 +0200

    Run RTD build without ROS dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ autosummary_generate = True
 numpydoc_show_class_members = False
 
 nbsphinx_kernel_name = "python3"
-nbsphinx_execute = "auto"
+nbsphinx_execute = os.environ.get("NBSPHINX_EXEC", "auto")
 nbsphinx_prolog = """
 .. nbinfo::
 

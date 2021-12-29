303e908691d11b34bbda727c1ab37e41f527ba7b
phausamann <peter.hausamann@tum.de>
2021-12-29 14:07:00 +0100

    Drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="Python utilities for estimating and transforming "
     "rigid body motion.",
